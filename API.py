135a3a50b6e09a3ba760cd57f9d8fdeb4b0513bc
isebarn <isebarn182@gmail.com>
2020-07-22 10:27:35 +0000

    Gunicorn

--- API.py
+++ API.py
@@ -1,10 +1,24 @@
-rom ORM import Operations
+from ORM import Operations
 from Parse import Ads
-from Discord import Discord
+from Discord import send_messages
 from flask import Flask, jsonify
 import asyncio
 
 app = Flask(__name__)
+
+@app.route('/update')
+def update():
+  sites = Operations.GetAllKeywords()
+  old_ads = Operations.GetAllIds()
+
+  ads = Ads(sites)
+  parsed_ads = ads.ads
+
+  new_ads = [ad for ad in parsed_ads if ad['id'] not in old_ads]
+  #[Operations.SaveAd(ad) for ad in new_ads]
+  send_messages([x.Readable() for x in Operations.GetAll()])
+  return jsonify([x.Readable() for x in Operations.GetAll()])
+
 
 
 @app.route('/getads')
--- Discord.py
+++ Discord.py
@@ -1,26 +1,24 @@
 import discord
 
-class Discord:
-  def __init__(self, ads):
-    self.client = discord.Client()
-    self.ads = ads
+def send_messages(ads):
+  client = discord.Client()
 
-    self.client.loop.create_task(self.my_background_task())
-    self.client.run('NzMyNzEwMzYxMDE0NDY4NzA2.Xw4j6Q.bV2KjT3_dCAGjosSTTv_7pYeEWI')
+  client.loop.create_task(my_background_task(client, ads))
+  client.run('NzMyNzEwMzYxMDE0NDY4NzA2.Xw4j6Q.bV2KjT3_dCAGjosSTTv_7pYeEWI')
 
-  async def my_background_task(self):
+async def my_background_task(client, ads):
 
-    for message in self.ads:
-      msg = '{}\n{}\n{}'.format(message['title'], message['payment'], message['url'])
-      await self.client.wait_until_ready()
-      channel = self.client.get_channel(732680171143954482)
-      await channel.send(msg)
+  for message in ads:
+    msg = '{}\n{}\n{}'.format(message['title'], message['payment'], message['url'])
+    await client.wait_until_ready()
+    channel = client.get_channel(732680171143954482)
+    await channel.send(msg)
 
-    await self.client.close()
+  await client.close()
 
 if __name__ == "__main__":
 
   ads = [{'id': '01d33e2d5b0742ea28', 'title': 'Lead generation to find MSPs in USLead generation to find MSPs in US', 'url': 'https://www.upwork.com/job/Lead-generation-find-MSPs_~01d33e2d5b0742ea28/', 'payment': 'Fixed: $10'}]
 
-  Discord(ads)
+  run(ads)
 
